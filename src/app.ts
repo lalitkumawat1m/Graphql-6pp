4dfa3da15703437cd7626dd90e24b500ccc1345c
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-10-20 10:16:28 +0530

    last commit

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,54 @@
-import express from 'express'
-import cors from 'cors'
-import {errorMiddleware} from './middlewares/error.js'
-import dotenv from 'dotenv'
+import { ApolloServer } from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+import dotenv from 'dotenv';
+import { schema } from './graphql/schema/schema.js';
 
 
   dotenv.config({path: './.env',});
 
   export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.PORT || 3000);
+
+const server = new ApolloServer({
+  typeDefs:schema,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello, World!',
+    },
+  },
+})
+
+startStandaloneServer(server,{
+  listen:{
+    port:port
+  }
+}).then(()=>{
+  console.log('Server is working on Port:'+port+' in '+envMode+' Mode.');
+})
+
+//   const app = express();
 
 
-  const app = express();
+//  app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use(cors({origin:' * ',credentials:true})); 
 
 
- app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors({origin:' * ',credentials:true})); 
+//   app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+//   });
 
-
-  app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-
-  // your routes here
+//   // your routes here
 
   
-  app.get("*", (req, res) => {
-    res.status(404).json({
-      success: false,
-      message: 'Page not found'
-    });
-  });
+//   app.get("*", (req, res) => {
+//     res.status(404).json({
+//       success: false,
+//       message: 'Page not found'
+//     });
+//   });
 
-  app.use(errorMiddleware);
+//   app.use(errorMiddleware);
   
   
-  app.listen(port, () => console.log('Server is working on Port:'+port+' in '+envMode+' Mode.'));
+  // app.listen(port, () => console.log('Server is working on Port:'+port+' in '+envMode+' Mode.'));
